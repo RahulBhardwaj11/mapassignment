fa273177d6c7efcdb06be22ae65dbb6aaaf6ae5b
Rahul Bhardwaj <56666595+RahulBhardwaj11@users.noreply.github.com>
2023-07-12 18:00:38 +0530

    InitialCode

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Map } from "./Map/Map";
+
+import "leaflet/dist/leaflet.css";
+import "./App.css";
+// import "antd/dist/antd.variable.min.css";
+// import "leaflet.markercluster/dist/MarkerCluster.css";
+// import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+// import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+
+const App = () => {
+  return <Map />;
+};
+export default App;
---
+++ src/Map/Map.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import {
+  LayersControl,
+  MapContainer,
+  TileLayer,
+  useMap,
+  Marker,
+  Popup,
+} from "react-leaflet";
+
+import icon from "./constants";
+import RoutineMachine from "../Map/RoutingMachine";
+import L from "leaflet";
+
+export const Map = () => {
+  function LocationMarker() {
+    const [position, setPosition] = useState(null);
+    const [bbox, setBbox] = useState([]);
+
+    const map = useMap();
+
+    useEffect(() => {
+      map.locate().on("locationfound", function (e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+        const radius = e.accuracy;
+        const circle = L.circle(e.latlng, radius);
+        circle.addTo(map);
+        setBbox(e.bounds.toBBoxString().split(","));
+      });
+    }, [map]);
+
+    return position === null ? null : (
+      <Marker position={position} icon={icon}>
+        <Popup>
+          You are here. <br />
+          Map bbox: <br />
+          <b>Southwest lng</b>: {bbox[0]} <br />
+          <b>Southwest lat</b>: {bbox[1]} <br />
+          <b>Northeast lng</b>: {bbox[2]} <br />
+          <b>Northeast lat</b>: {bbox[3]}
+        </Popup>
+      </Marker>
+    );
+  }
+
+  return (
+    <MapContainer center={[0, 0]} zoom={1} scrollWheelZoom={true}>
+      <LayersControl position="topright">
+        <LayersControl.BaseLayer checked name="OSM Streets">
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </LayersControl.BaseLayer>
+      </LayersControl>
+      <LocationMarker />
+      <RoutineMachine/>
+    </MapContainer>
+  );
+};
